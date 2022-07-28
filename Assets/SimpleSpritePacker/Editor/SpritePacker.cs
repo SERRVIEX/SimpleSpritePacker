1185561647038ba02b24961698c8805a50893516
SERRVIEX <48734641+SERRVIEX@users.noreply.github.com>
2022-07-28 11:15:14 +0300

    Moved some draw editor styles in the other script.

--- Assets/SimpleSpritePacker/Editor/SpritePacker.cs
+++ Assets/SimpleSpritePacker/Editor/SpritePacker.cs
@@ -62,7 +62,7 @@ namespace SimpleSpritePacker
 
         private void OnGUI()
         {
-            InitializeStyles();
+            SimpleEditor.Initialize();
 
             EditorGUILayout.BeginVertical();
             _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, GUILayout.Width(position.width), GUILayout.Height(position.height));
@@ -92,18 +92,18 @@ namespace SimpleSpritePacker
 
             EditorGUILayout.LabelField($"Area Filled {_filledArea}%", new GUIStyle("helpBox"));
 
-            TitleBar("Properties");
+            SimpleEditor.Header("Properties");
 
             _width = EditorGUILayout.IntField("Width", Mathf.Clamp(_width, 1, 4096));
             _height = EditorGUILayout.IntField("Height", Mathf.Clamp(_height, 1, 4096));
             _spacing = EditorGUILayout.IntField("Spacing", _spacing);
 
-            TitleBar("Conditions");
+            SimpleEditor.Header("Conditions");
 
             _minSpriteSize = EditorGUILayout.Vector2IntField("Min Sprite Size", _minSpriteSize);
             _maxSpriteSize = EditorGUILayout.Vector2IntField("Max Sprite Size", _maxSpriteSize);
 
-            TitleBar("Inputs");
+            SimpleEditor.Header("Inputs");
 
             ScriptableObject target = this;
             SerializedObject serializedObject = new SerializedObject(target);
@@ -199,12 +199,12 @@ namespace SimpleSpritePacker
 
             if (_sourceAtlas == null)
             {
-                TitleBar("Export");
+                SimpleEditor.Header("Export");
                 _outputName = EditorGUILayout.TextField("Name", _outputName);
                 _outputSpritesPrefixName = EditorGUILayout.TextField("Sprites Prefix", _outputSpritesPrefixName);
             }
 
-            TitleBar("Actions");
+            SimpleEditor.Header("Actions");
 
             EditorGUILayout.PropertyField(serializedObject.FindProperty("_algorithmType"), true);
 
@@ -221,7 +221,7 @@ namespace SimpleSpritePacker
 
             if (_unpackedTextures.Count > 0)
             {
-                TitleBar($"Not packed ({_unpackedTextures.Count} from {_textures.Count})");
+                SimpleEditor.Header($"Not packed ({_unpackedTextures.Count} from {_textures.Count})");
 
                 for (int i = 0; i < _unpackedTextures.Count; i++)
                     GUILayout.Label($"{_unpackedTextures[i].Texture.name} - ({_unpackedTextures[i].Texture.width}x{_unpackedTextures[i].Texture.height})");
@@ -466,65 +466,6 @@ namespace SimpleSpritePacker
                 AssetDatabase.Refresh();
                 return _sourceAtlas;
             }
-        }
-
-        private void InitializeStyles()
-        {
-            if (_headerStyle == null)
-            {
-                _headerStyle = new GUIStyle(GUI.skin.box);
-                _headerStyle.normal.textColor = Color.white;
-                _headerStyle.normal.background = CreateTexture(2, 2, new Color32(62, 62, 62, 255));
-            }
-        }
-
-        private Texture2D CreateTexture(int width, int height, Color color)
-        {
-            Color[] pixels = new Color[width * height];
-            for (int i = 0; i < pixels.Length; ++i)
-                pixels[i] = color;
-
-            Texture2D result = new Texture2D(width, height);
-            result.SetPixels(pixels);
-            result.Apply();
-            return result;
-        }
-
-        private void TitleBar(string title)
-        {
-            EditorGUILayout.Space(5);
-
-            BeginHorizontalLine();
-
-            Rect rect = EditorGUILayout.GetControlRect(false, 20);
-            rect.position = new Vector2(0, rect.position.y);
-            GUIStyle titleStyle = new GUIStyle(_headerStyle);
-            titleStyle.fontStyle = FontStyle.Bold;
-            titleStyle.fixedWidth = EditorGUIUtility.currentViewWidth;
-            titleStyle.stretchWidth = true;
-            EditorGUI.LabelField(rect, title, titleStyle);
-
-            EndHorizontalLine();
-        }
-
-        private void BeginHorizontalLine()
-        {
-            Rect rect = EditorGUILayout.GetControlRect(false, -1);
-            rect.position = new Vector2(0, rect.position.y);
-            rect.width = EditorGUIUtility.currentViewWidth;
-            rect.height = 1;
-
-            EditorGUI.DrawRect(rect, new Color32(26, 26, 26, 255));
-        }
-
-        private void EndHorizontalLine()
-        {
-            Rect rect = EditorGUILayout.GetControlRect(false, 1);
-            rect.position = new Vector2(0, rect.position.y - 2);
-            rect.width = EditorGUIUtility.currentViewWidth;
-            rect.height = 1;
-
-            EditorGUI.DrawRect(rect, new Color32(48, 48, 48, 255));
         }
     }
 }
